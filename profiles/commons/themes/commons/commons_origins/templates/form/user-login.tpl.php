007e67acf5be1ece4cb6116560a244b67bc3404d
aleverenz <leverenz@uta.edu>
2018-05-07 15:44:30 -0500

    Revert "update template"
    This reverts commit abacb840b71dc7e8f34d9928296b136b774084a1.

--- profiles/commons/themes/commons/commons_origins/templates/form/user-login.tpl.php
+++ profiles/commons/themes/commons/commons_origins/templates/form/user-login.tpl.php
@@ -6,12 +6,6 @@
   print_r($variables);
   print '</pre>';
 
-//print drupal_render_children($form) 
-
- print drupal_render($form['name']);
-    print drupal_render($form['pass']);
-    print drupal_render($form['form_build_id']);
-    print drupal_render($form['form_id']);
-    print drupal_render($form['actions']);
+print drupal_render_children($form) 
 
 ?>
